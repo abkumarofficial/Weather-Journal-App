35763f94124662a80b433ddc3a5b994f548999e2
Abhijeet Kumar <abkumarofficial@gmail.com>
2021-06-10 16:13:45 -0700

    Completed backend part

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {
+    name : 'Abhijeet',
+    temp : '294.13',
+    date : '5.11.2021',
+    content : 'Feeling Cold',
+};
 
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser')
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
+
+app.get('/getdata', function (req, res) {
+    console.log('get request came', projectData)
+    res.send(projectData);
+  })
+
+app.post('/postdata', (req, res) => {
+    projectData = req.body;
+    console.log('data recieved by server', req.body);
+    console.log('Project data set', projectData);
+    res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,63 @@
+// const { json } = require("body-parser");
+
 /* Global Variables */
+const apiKey = "cbd3f47cb61f72bbc59cf7f19ede7cb5";
+const baseURL = "api.openweathermap.org/data/2.5/weather";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.querySelector('#generate').addEventListener('click', getDataByZipCode);
+
+function getDataByZipCode(){
+    const zipCode = document.querySelector('#zip').value;
+    const countryCode = document.querySelector('#country').value;
+    const content = document.querySelector('#feelings').value;
+    if ( zipCode.length > 0 && countryCode.length > 0 && !isNaN(zipCode)) {
+        // Calling Web API
+        getDataFromWebAPI(zipCode, countryCode)
+        .then((data) => postData(data));
+    } else {
+        alert('Please Verify Zipcode or Country Code that you have provided');
+    }
+    
+    }
+
+// This Function is to get the data from  WEB API
+const getDataFromWebAPI = async (zipCode, countryCode)=>{
+    const searchURL = `https://${baseURL}?zip=${zipCode},${countryCode}&appid=${apiKey}`
+    const res = await fetch(searchURL);
+    try {
+        const data = await res.json();
+        return data;
+    }  catch(error) {
+        console.log("error", error);
+    }
+}
+
+const postData = async (data) => {
+    const postData = {
+        name: data.name !== undefined ? data.name : 'Name not Found',
+        date: newDate,
+        temp: data.main.temp !== undefined ? data.main.temp : 'Data not Found',
+        content: content,
+    }
+    const response = fetch('/postdata', {
+        method: 'POST',
+        credentials:'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(() => {
+        getData();
+    });
+}
+
+const getData = async () => {
+    const response =await fetch('/getdata')
+    .then((response) => response.json())
+    .then((responseJSON) => console.log('responseJson', responseJSON))
+}
